011ba7643a09f5aeb098c5cb2cfc26b06a991d3f
jarwonozt <jarwonoztaveiro@gmail.com>
2022-05-31 22:03:01 +0700

    live search by code certificate -  stuck blank after search

--- app/Http/Livewire/CertificateSearch.php
+++ app/Http/Livewire/CertificateSearch.php
@@ -3,11 +3,33 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class CertificateSearch extends Component
 {
+    public $search;
+    protected $queryString = ['search'=> ['except' => '']];
+
+
     public function render()
     {
-        return view('livewire.certificate-search');
+        // dd($certificate->json());
+        // $posts = Post::latest()->paginate($this->limitPerPage);
+        $data = array();
+
+        if ($this->search !== null) {
+            $certificate = Http::get('https://g-academy.net/api/certificate/'.$this->search);
+                if($certificate->successful()){
+                    $data = $certificate->json();
+                    // dd($certificate->json());
+                    // dd($data['data']['certificate']['user']['name']);
+                }else{
+                    $data = 0;
+                }
+        }
+
+        $this->search == null;
+
+        return view('livewire.certificate-search', ['data' => $data]);
     }
 }
--- resources/views/livewire/certificate-search.blade.php
+++ resources/views/livewire/certificate-search.blade.php
@@ -14,41 +14,16 @@
         <div class="container">
             <div class="doc_banner_content">
                 <h2 class="wow fadeInUp">How can we help you?</h2>
-                <p class="wow fadeInUp" data-wow-delay="0.2s">Search here to get answers to your questions</p>
-                <form action="#" class="header_search_form">
+                <p class="wow fadeInUp" data-wow-delay="0.2s">Enter the certificate code here</p>
+                <form class="header_search_form">
                     <div class="header_search_form_info">
                         <div class="form-group">
                             <div class="input-wrapper">
                                 <i class="icon_search"></i>
-                                <input type='search' id="searchbox" autocomplete="off" name="search"
+                                <input type='search' id="searchbox" wire:model="search" autocomplete="off"
                                     placeholder="Search the Doc" />
-                                <div class="header_search_form_panel">
-                                    <ul class="list-unstyled">
-                                        <li>Help Desk
-                                            <ul class="list-unstyled search_item">
-                                                <li><span>Configuration</span><a href="#">How to edit host and
-                                                        port?</a></li>
-                                                <li><span>Configuration</span><a href="#">The dev Property</a></li>
-                                            </ul>
-                                        </li>
-                                        <li>Support
-                                            <ul class="list-unstyled search_item">
-                                                <li><span>Pages</span><a href="#">The asyncData Method</a></li>
-                                            </ul>
-                                        </li>
-                                        <li>Documentation
-                                            <ul class="list-unstyled search_item">
-                                                <li><span>Getting Started</span><a href="#">The asyncData Method</a>
-                                                </li>
-                                                <li><span>Getting Started</span><a href="#">The asyncData Method</a>
-                                                </li>
-                                                <li><span>Getting Started</span><a href="#">The asyncData Method</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <select class="search-expand-types custom-select" name="post_type"
+
+                                {{-- <select class="search-expand-types custom-select" name="post_type"
                                     id="search_post_type">
                                     <option value="">All Docs </option>
                                     <option value="manual_kb">Knowledge Base</option>
@@ -57,19 +32,11 @@
                                     <option value="forum">Forums</option>
                                     <option value="manual_portfolio">Portfolio</option>
                                     <option value="product">Products</option>
-                                </select>
+                                </select> --}}
                             </div>
                             <button type="submit" class="header_form_submit">Search</button>
                         </div>
 
-                    </div>
-                    <div class="header_search_keyword">
-                        <span class="header-search-form__keywords-label">Suggested Search:</span>
-                        <ul class="list-unstyled">
-                            <li class="wow fadeInUp" data-wow-delay="0.2s"><a href="#">guest users</a></li>
-                            <li class="wow fadeInUp" data-wow-delay="0.3s"><a href="#">invoice</a></li>
-                            <li class="wow fadeInUp" data-wow-delay="0.4s"><a href="#">security</a></li>
-                        </ul>
                     </div>
                 </form>
             </div>
@@ -78,90 +45,69 @@
     <section class="doc_features_area">
         <img class="doc_features_shap" src="{{asset('assets')}}/img/new/shap_white.png" alt="">
         <div class="container">
-            <div class="doc_features_inner">
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.1s" data-wow-duration="0.5s">
-                    <img src="{{asset('assets')}}/img/new/icon1.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Knowledge Base</h4>
-                        </a>
-                        <p>245 Posts</p>
-                    </div>
+            @if ($data == 0)
+                <div class="doc_features_inner p-4">
+                        Data not found!
                 </div>
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="0.6s">
-                    <img src="{{asset('assets')}}/img/new/icon2.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Community Forums</h4>
-                        </a>
-                        <p>245 Posts</p>
+            @endif
+
+            @if (empty($data))
+                <div class="doc_features_inner mt-2">
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.1s" data-wow-duration="0.5s">
+                        <img src="{{asset('assets')}}/img/new/icon1.png" alt="">
+                        <div class="media-body">
+                            <a href="#">
+                                <h4>Knowledge Base</h4>
+                            </a>
+                            <p>245 Posts</p>
+                        </div>
                     </div>
-                </div>
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.3s" data-wow-duration="0.7s">
-                    <img src="{{asset('assets')}}/img/new/icon3.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Documentation</h4>
-                        </a>
-                        <p>245 Posts</p>
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="0.6s">
+                        <img src="{{asset('assets')}}/img/new/icon2.png" alt="">
+                        <div class="media-body">
+                            <a href="#">
+                                <h4>Community Forums</h4>
+                            </a>
+                            <p>245 Posts</p>
+                        </div>
                     </div>
-                </div>
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="0.8s">
-                    <img src="{{asset('assets')}}/img/new/icon4.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Working with Docs</h4>
-                        </a>
-                        <p>245 Posts</p>
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.3s" data-wow-duration="0.7s">
+                        <img src="{{asset('assets')}}/img/new/icon3.png" alt="">
+                        <div class="media-body">
+                            <a href="#">
+                                <h4>Documentation</h4>
+                            </a>
+                            <p>245 Posts</p>
+                        </div>
                     </div>
-                </div>
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="0.9s">
-                    <img src="{{asset('assets')}}/img/new/icon5.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Getting Started</h4>
-                        </a>
-                        <p>245 Posts</p>
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="0.8s">
+                        <img src="{{asset('assets')}}/img/new/icon4.png" alt="">
+                        <div class="media-body">
+                            <a href="#">
+                                <h4>Working with Docs</h4>
+                            </a>
+                            <p>245 Posts</p>
+                        </div>
                     </div>
-                </div>
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="1s">
-                    <img src="{{asset('assets')}}/img/new/icon6.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Account Management</h4>
-                        </a>
-                        <p>245 Posts</p>
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="0.9s">
+                        <img src="{{asset('assets')}}/img/new/icon5.png" alt="">
+                        <div class="media-body">
+                            <a href="#">
+                                <h4>Getting Started</h4>
+                            </a>
+                            <p>245 Posts</p>
+                        </div>
                     </div>
-                </div>
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="0.7s">
-                    <img src="{{asset('assets')}}/img/new/icon7.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Productivity</h4>
-                        </a>
-                        <p>245 Posts</p>
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="1s">
+                        <img src="{{asset('assets')}}/img/new/icon6.png" alt="">
+                        <div class="media-body">
+                            <a href="#">
+                                <h4>Account Management</h4>
+                            </a>
+                            <p>245 Posts</p>
+                        </div>
                     </div>
-                </div>
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="0.8s">
-                    <img src="{{asset('assets')}}/img/new/icon8.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Docs in Help Scout</h4>
-                        </a>
-                        <p>245 Posts</p>
-                    </div>
-                </div>
-                <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="0.9s">
-                    <img src="{{asset('assets')}}/img/new/icon9.png" alt="">
-                    <div class="media-body">
-                        <a href="#">
-                            <h4>Formatting Content</h4>
-                        </a>
-                        <p>245 Posts</p>
-                    </div>
-                </div>
-                <div class="see_more_item collapse-wrap">
-                    <div class="media doc_features_item">
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="0.7s">
                         <img src="{{asset('assets')}}/img/new/icon7.png" alt="">
                         <div class="media-body">
                             <a href="#">
@@ -170,7 +116,7 @@
                             <p>245 Posts</p>
                         </div>
                     </div>
-                    <div class="media doc_features_item">
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="0.8s">
                         <img src="{{asset('assets')}}/img/new/icon8.png" alt="">
                         <div class="media-body">
                             <a href="#">
@@ -179,7 +125,7 @@
                             <p>245 Posts</p>
                         </div>
                     </div>
-                    <div class="media doc_features_item">
+                    <div class="media doc_features_item wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="0.9s">
                         <img src="{{asset('assets')}}/img/new/icon9.png" alt="">
                         <div class="media-body">
                             <a href="#">
@@ -188,12 +134,45 @@
                             <p>245 Posts</p>
                         </div>
                     </div>
+                    <div class="see_more_item collapse-wrap">
+                        <div class="media doc_features_item">
+                            <img src="{{asset('assets')}}/img/new/icon7.png" alt="">
+                            <div class="media-body">
+                                <a href="#">
+                                    <h4>Productivity</h4>
+                                </a>
+                                <p>245 Posts</p>
+                            </div>
+                        </div>
+                        <div class="media doc_features_item">
+                            <img src="{{asset('assets')}}/img/new/icon8.png" alt="">
+                            <div class="media-body">
+                                <a href="#">
+                                    <h4>Docs in Help Scout</h4>
+                                </a>
+                                <p>245 Posts</p>
+                            </div>
+                        </div>
+                        <div class="media doc_features_item">
+                            <img src="{{asset('assets')}}/img/new/icon9.png" alt="">
+                            <div class="media-body">
+                                <a href="#">
+                                    <h4>Formatting Content</h4>
+                                </a>
+                                <p>245 Posts</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#more-features" class="collapse-btn see_btn">
+                        <i class="arrow_carrot-down_alt2"></i>
+                        <span class="text">Show More</span>
+                    </a>
                 </div>
-                <a href="#more-features" class="collapse-btn see_btn">
-                    <i class="arrow_carrot-down_alt2"></i>
-                    <span class="text">Show More</span>
-                </a>
-            </div>
+            @else
+                <div class="doc_features_inner p-4">
+                    {{ $data['data']['certificate']['user']['name'] }}
+                </div>
+            @endif
         </div>
     </section>
 </div>
